9ee1b9b181367767888eced5846c98bf00dc8265
rventur <35280883+rventur@users.noreply.github.com>
2018-08-29 13:55:21 -0700

    Create SayHello file

---
+++ Classwork/Classwork01/SayHello.java
@@ -0,0 +1,29 @@
+/*  File name     :  SayHello.java
+ *  Purpose       :  Provides a class with supporting methods for CountTheDays.java program
+ *  Authors       :  Roberto Ventura , B.J. Johnson (for Template)
+ *  Date          :  2018-01-25 
+ *  Description   :  Practice using scanner utility
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ----------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *   @version 1.0.0  2018-8-29  Bob           Practice using Java and scanner utility
+ */
+   import java.util.Scanner;
+   
+   public class SayHello {
+	   
+       public static void main(String[] args) {
+		   
+	       String h = "Hello";
+	       System.out.println(h + " World!");
+   	       System.out.print("What is your name?: ");
+		   Scanner myInput = new Scanner( System.in );
+		   String inputName = myInput.nextLine();
+		   System.out.println(h + ", " + inputName);
+		   
+       }
+   }
